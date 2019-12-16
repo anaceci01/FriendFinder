7c2f16a65c7c76bd39c6461d1541ba7e2998ea8c
Ana Davis <anac.davis@gmail.com>
2019-12-15 23:05:44 -0700

    rout te home  ad survey were not funcionally working, It was missing the app callback

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.get(express.static("app/public"));
 
-require("./app/routing/apiRoutes.js") //(app);
-require("./app/routing/htmlRoutes.js") //(app);
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
 
 
 app.listen(port, () => console.log("listening on port ", port));
