cdd3a17c5fc097539fc636ee95c50dc5103b000c
Ana Davis <anac.davis@gmail.com>
2019-12-25 23:16:56 -0700

    survey button properly working. added bodyParser

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,10 @@
 let path = require("path");
 
 module.exports = function(app) {
-    app.get("/survey", (req, res) => {
-        res.sendFile(path.join(__dirname + "../public/survey.html"));
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
-    app.use("/", (req, res) => {
+    app.use(function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 }
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require("express");
-var app = express()
 var bodyParser = require("body-parser");
-
 var path = require("path");
 
+var app = express();
 var PORT = process.env.PORT || 3000;
 
 
 //create app/json parser
-var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//var jsonParser = bodyParser.json()
+//var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(bodyParser.json({ type: 'application/**json' }))
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }))
 app.use(bodyParser.text({ type: 'text/html' }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 require("./app/routing/htmlRoutes.js")(app);
